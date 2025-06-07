7e4846c2010770fbc0f28cd05ad87c7503114967
Mikhail Bakulev <michael.baculev@gmail.com>
2025-06-07 17:39:26 +0400

    add гонка бегунов

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,44 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        AppConfig config = new AppConfig();
+
+        int barriersCount = Integer.parseInt(config.getProperty("app.runners"));
+        int runnersCount = Integer.parseInt(config.getProperty("app.barrriers"));
+
+        Runner[] runners = new Runner[runnersCount];
+        Thread[] runnerThreads = new Thread[runnersCount];
+
+        for (int i = 0; i < runnersCount; i++) {
+            runners[i] = new Runner(barriersCount);
+            runnerThreads[i] = new Thread(runners[i]);
+        }
+
+        for (int i = 0; i < runnersCount; i++) {
+            runnerThreads[i].start();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            runnerThreads[i].join();
+        }
+
+        Runner winner = runners[0];
+        Runner loser = runners[0];
+
+        for (int i = 0; i < 3; i++) {
+            if (runners[i].getTimeOut() < winner.getTimeOut()) {
+                winner = runners[i];
+            }
+            if (runners[i].getTimeOut() > loser.getTimeOut()) {
+                loser = runners[i];
+            }
+        }
+
+        // Вывод результатов
+        System.out.println("Победитель: Спортсмен № " + winner.getId());
+        System.out.println("Аутсайдер: Спортсмен № " + loser.getId());
+
+
     }
 }
